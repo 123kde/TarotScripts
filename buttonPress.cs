e8d976bc25f059d507024dd7e65645f3762c9f38
123kde <62267818+123kde@users.noreply.github.com>
2021-04-07 13:40:47 -0400

    Update buttonPress.cs
    trimmed script, added 'to do' in regards to adding sounds for event

--- buttonPress.cs
+++ buttonPress.cs
@@ -1,30 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class buttonPress : MonoBehaviour
 {
-
     public AnimatorControllerParameter cupola;
     public Animator characterAnimator;
-
-
     private void OnCollisionEnter(Collision collision)
     {
-        print("HIT");
         characterAnimator.SetBool("buttonPushed", true);
-    }
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //code shield opening and button click sounds here
     }
 }
