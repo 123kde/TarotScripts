8b3fc2b83685ebf81ea667db5b447f9a4f875343
123kde <62267818+123kde@users.noreply.github.com>
2021-04-07 13:12:54 -0400

    Update glassCollisionSFX.cs
    made logical flow of script more professional (I think)

--- glassCollisionSFX.cs
+++ glassCollisionSFX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class glassCollisionSFX : MonoBehaviour
 {
     public AudioSource audio;
-    public int loudSoundMagnitude = 3;
+    public double loudSoundMagnitude = 2.5;
     public AudioClip knock1, knock2, knock3, knock4, knock5, tap1, tap2, tap3, tap4, tap5;
     float knockCooldown = 0.15f;
 
@@ -14,45 +14,74 @@ public class glassCollisionSFX : MonoBehaviour
         //print(collision);
         //print(collision.relativeVelocity.magnitude);
         int random = Random.Range(1, 6);
-        
-        if(collision.relativeVelocity.magnitude < 0.5)
+
+        if (collision.relativeVelocity.magnitude < 0.5)
         {
+            knockCooldown = 0.15f;
             return;
         }
 
-        if(collision.relativeVelocity.magnitude >= 3 && knockCooldown <= 0)
+        if (knockCooldown <= 0)
+        {
+            switch (random)
+            {
+                case 1:
+                    audio.PlayOneShot(collision.relativeVelocity.magnitude >= loudSoundMagnitude ? knock1 : tap1);
+                    //print(random + "knock1");
+                    break;
+                case 2:
+                    audio.PlayOneShot(collision.relativeVelocity.magnitude >= loudSoundMagnitude ? knock2 : tap2);
+                    //print(random + "knock2");
+                    break;
+                case 3:
+                    audio.PlayOneShot(collision.relativeVelocity.magnitude >= loudSoundMagnitude ? knock3 : tap3);
+                    //print(random + "knock3");
+                    break;
+                case 4:
+                    audio.PlayOneShot(collision.relativeVelocity.magnitude >= loudSoundMagnitude ? knock4 : tap4);
+                    //print(random + "knock4");
+                    break;
+                case 5:
+                    audio.PlayOneShot(collision.relativeVelocity.magnitude >= loudSoundMagnitude ? knock5 : tap5);
+                    //print(random + "knock5");
+                    break;
+                default://should never reach here but whatever
+                    audio.PlayOneShot(collision.relativeVelocity.magnitude >= loudSoundMagnitude ? knock5 : tap5);
+                    //print(random + "defaultknock");
+                    break;
+
+            }
+        }
+
+        knockCooldown = 0.15f;
+
+        /*if(collision.relativeVelocity.magnitude >= 3 && knockCooldown <= 0)
         {
             switch (random)
             {
                 case 1:
                     audio.PlayOneShot(knock1);
-                    print(random + "knock1");
-                    knockCooldown = 0.15f;
+                    //print(random + "knock1");
                     break;
                 case 2:
                     audio.PlayOneShot(knock2);
-                    print(random + "knock2");
-                    knockCooldown = 0.15f;
+                    //print(random + "knock2");
                     break;
                 case 3:
                     audio.PlayOneShot(knock3);
-                    print(random + "knock3");
-                    knockCooldown = 0.15f;
+                    //print(random + "knock3");
                     break;
                 case 4:
                     audio.PlayOneShot(knock4);
-                    print(random + "knock4");
-                    knockCooldown = 0.15f;
+                    //print(random + "knock4");
                     break;
                 case 5:
                     audio.PlayOneShot(knock5);
-                    print(random + "knock5");
-                    knockCooldown = 0.15f;
+                    //print(random + "knock5");
                     break;
                 default://should never reach here but whatever
                     audio.PlayOneShot(knock5);
-                    knockCooldown = 0.15f;
-                    print(random + "defaultknock");
+                    //print(random + "defaultknock");
                     break;
 
             }
@@ -63,37 +92,31 @@ public class glassCollisionSFX : MonoBehaviour
             {
                 case 1:
                     audio.PlayOneShot(tap1);
-                    print(random + "tap1");
-                    knockCooldown = 0.15f;
+                    //print(random + "tap1");
                     break;
                 case 2:
                     audio.PlayOneShot(tap2);
-                    print(random + "tap2");
-                    knockCooldown = 0.15f;
+                    //print(random + "tap2");
                     break;
                 case 3:
                     audio.PlayOneShot(tap3);
-                    print(random + "tap3");
-                    knockCooldown = 0.15f;
+                    //print(random + "tap3");
                     break;
                 case 4:
                     audio.PlayOneShot(tap4);
-                    print(random + "tap4");
-                    knockCooldown = 0.15f;
+                    //print(random + "tap4");
                     break;
                 case 5:
                     audio.PlayOneShot(tap5);
-                    print(random + "tap5");
-                    knockCooldown = 0.15f;
+                    //print(random + "tap5");
                     break;
                 default://should never reach here but whatever
                     audio.PlayOneShot(tap5);
-                    print(random + "defaulttap");
-                    knockCooldown = 0.15f;
+                    //print(random + "defaulttap");
                     break;
 
             }
-        }
+        }*/
 
 
     }
