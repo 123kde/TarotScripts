3bbab0a2b0f4a79d985ba63ce5386e5f140db131
123kde <62267818+123kde@users.noreply.github.com>
2021-04-07 23:12:13 -0400

    switchScene created
    does not do as the name suggests, yet. was merely a proof of concept to prove i can trigger an activity on object grab.  next will come the actual scene switching

---
+++ switchScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchScene : MonoBehaviour
+{
+
+    public AudioSource audio;
+    public AudioClip daBaby;
+
+    public void LetsGo()
+    {
+        audio.PlayOneShot(daBaby);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
