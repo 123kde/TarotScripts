e5e24e274e0558cc2072c4e7fd7d6b403a41aac2
123kde <62267818+123kde@users.noreply.github.com>
2021-04-08 21:32:45 -0400

    Update switchScene.cs
    added audiovisual content for the scene switch, as well as an initial fade in from white (this will probably need to be moved to another script and put on a random game item at some point

--- switchScene.cs
+++ switchScene.cs
@@ -1,21 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class switchScene : MonoBehaviour
 {
 
     public AudioSource audio;
-    public AudioClip daBaby;
+    public AudioClip transitionSound;
+    public float _fadeDuration = 2.5f;
 
     public void LetsGo()
     {
-        audio.PlayOneShot(daBaby);
+        audio.PlayOneShot(transitionSound);
+        FadeToWhite();
+        Invoke("ActuallySwitchScene", 3);
     }
+
+
+    void ActuallySwitchScene()
+    {
+        FadeFromWhite();
+        //switchscene
+    }
+
+    //following 2 functions taken from:https://answers.unity.com/questions/1258342/steam-vr-fade-camera.html, answer by dsentence
+
+    private void FadeToWhite()
+    {
+        //set start color
+        SteamVR_Fade.Start(Color.clear, 0f);
+        //set and start fade to
+        SteamVR_Fade.Start(Color.white, _fadeDuration);
+    }
+    private void FadeFromWhite()
+    {
+        //set start color
+        SteamVR_Fade.Start(Color.white, 0f);
+        //set and start fade to
+        SteamVR_Fade.Start(Color.clear, _fadeDuration);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        FadeFromWhite();
     }
 
     // Update is called once per frame
