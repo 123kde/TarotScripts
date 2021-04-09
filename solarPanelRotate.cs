b1b1ba47c36ac34b155057b2eac4d0a4b1d25323
123kde <62267818+123kde@users.noreply.github.com>
2021-04-09 02:49:46 -0400

    made solar panels track the sun
    success! works perfectly!

---
+++ solarPanelRotate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class solarPanelRotate : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        transform.Rotate(new Vector3((float)-0.1, 0, 0) * Time.deltaTime/*, Space.World*/);
+    }
+}
