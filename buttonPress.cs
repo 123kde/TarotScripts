9f1da5ef8fd4200163666137e0d37205fa1cfd5c
123kde <62267818+123kde@users.noreply.github.com>
2021-04-09 05:05:47 -0400

    Update buttonPress.cs
    changed button functionality to not simply be a collision, also implemented sound on the button and shields opening

--- buttonPress.cs
+++ buttonPress.cs
@@ -6,10 +6,54 @@ public class buttonPress : MonoBehaviour
 {
     public AnimatorControllerParameter cupola;
     public Animator characterAnimator;
+    public AudioSource button, shield;
+    public AudioClip buttonClick, motors;
+    bool pushed = false;
+    bool down = false;
 
+    public void press()
+    {
+        if (!down)
+        {
+            button.PlayOneShot(buttonClick);
+            down = true;
+        }
+        if (!pushed)
+        {
+            pushed = true;
+            characterAnimator.SetBool("buttonPushed", true);
+            Invoke("playMotorSound", 4.5f);
+        }
+    }
+
+    /*
     private void OnCollisionEnter(Collision collision)
     {
-        characterAnimator.SetBool("buttonPushed", true);
-        //code shield opening and button click sounds here
+        if (!pushed)
+        {
+            pushed = true;
+            characterAnimator.SetBool("buttonPushed", true);
+
+            button.PlayOneShot(buttonClick);
+
+            Invoke("playMotorSound", 20);
+        }
+    }
+    */
+
+    private void playMotorSound()
+    {
+        shield.PlayOneShot(motors);
+        Invoke("stopMotorSound", 4);
+    }
+
+    private void stopMotorSound()
+    {
+        shield.Stop();
+    }
+
+    public void setDownFalse()
+    {
+        down = false;
     }
 }
