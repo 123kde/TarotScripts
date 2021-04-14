8ad32086308d96918e8a607808fa390e687951df
123kde <62267818+123kde@users.noreply.github.com>
2021-04-14 18:26:28 -0400

    Update appleScript.cs
    updated to final functionality

--- appleScript.cs
+++ appleScript.cs
@@ -4,11 +4,48 @@ using UnityEngine;
 
 public class appleScript : MonoBehaviour
 {
+    public AudioSource appleSound;
+    public AudioClip appleCrunch, appleImpact;
+    public double soundMagnitude = 0.75;
+    public float knockCooldown = 0.15f;
+    float originalKnockCooldown;
+    bool held = false;
+
+    public void setHeldTrue()
+    {
+        held = true;
+    }
+
+    public void setHeldFalse()
+    {
+        held = false;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "HeadCollider")
+        if (held && collision.gameObject.name == "HeadCollider")
         {
+            appleSound.PlayOneShot(appleCrunch);
             Destroy(this.gameObject);
+        }
+        else if (knockCooldown <= 0 && collision.relativeVelocity.magnitude >= soundMagnitude)
+        {
+            appleSound.PlayOneShot(appleImpact);
+        }
+
+        knockCooldown = originalKnockCooldown;
+    }
+
+    void Start()
+    {
+        originalKnockCooldown = knockCooldown;
+    }
+
+    void FixedUpdate()
+    {
+        if (knockCooldown >= 0)
+        {
+            knockCooldown -= Time.deltaTime;
         }
     }
 }
